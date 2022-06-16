78a07602a6ae1d3dd501bcbad5461dff87bbd8df
Vincent Wei <vincent@minigui.org>
2022-06-16 10:39:37 +0800

    rearrange asset files for samples

--- source/bin/purcsex/calculator/calculator.c
+++ source/bin/purcsex/calculator/calculator.c
@@ -20,9 +20,9 @@
 ** along with this program.  If not, see http://www.gnu.org/licenses/.
 */
 
-#include "purcsex.h"
 #include "calculator.h"
-#include "log.h"
+#include "../purcsex.h"
+#include "../log.h"
 
 #include <stdio.h>
 #include <stdlib.h>

a89c0aeac08365d7e6cda810b344f011bc74977b
Vincent Wei <vincent@minigui.org>
2022-06-16 12:07:44 +0800

    assets for sample navigation

---
+++ source/bin/purcsex/navigation/navigation.c
@@ -0,0 +1,41 @@
+/*
+** handlers.c - event handlers for sample navigation
+**
+** Copyright (C) 2022 FMSoft (http://www.fmsoft.cn)
+**
+** Author: Vincent Wei (https://github.com/VincentWei)
+**
+** This file is part of PurC Midnight Commander.
+**
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
+
+#include "navigation.h"
+#include "../purcsex.h"
+#include "../log.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+
+void calc_click_tab(pcrdr_conn* conn,
+        purc_variant_t event_desired, const pcrdr_msg *event_msg)
+{
+    (void)conn;
+    (void)event_desired;
+    (void)event_msg;
+}
+
---
+++ source/bin/purcsex/navigation/navigation.h
@@ -0,0 +1,35 @@
+/*
+** navigation.h - event handlers for navigation
+**
+** Copyright (C) 2022 FMSoft (http://www.fmsoft.cn)
+**
+** Author: Vincent Wei (https://github.com/VincentWei)
+**
+** This file is part of PurC Midnight Commander.
+**
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
+
+#include <purc/purc.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void nav_click_tab(pcrdr_conn* conn,
+        purc_variant_t event_desired, const pcrdr_msg *event_msg);
+
+#ifdef __cplusplus
+}
+#endif
+
